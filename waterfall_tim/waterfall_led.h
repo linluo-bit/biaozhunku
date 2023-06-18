fe8240ec80ea2c41d3da0fc5ce090e67a8a86373
linluo-bit <53107393+linluo-bit@users.noreply.github.com>
2023-06-18 19:16:44 +0800

    Create waterfall_led.h

---
+++ waterfall_tim/waterfall_led.h
@@ -0,0 +1,10 @@
+#ifndef WATERFALL_LED_H
+#define WATERFALL_LED_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+void LED_Waterfall_Init(void);
+void LED_Waterfall_Update(void);
+
+#endif /* WATERFALL_LED_H */
