2216aebea7724874c4ecf4946a7cbf91909c74f1
linluo-bit <53107393+linluo-bit@users.noreply.github.com>
2023-06-18 23:48:38 +0800

    Create tim_keyboard.h

---
+++ keyboard/tim_keyboard.h
@@ -0,0 +1,21 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#include "stm32f10x.h"
+
+#define ROWS 4
+#define COLS 4
+
+extern GPIO_TypeDef* ROW_PORT[ROWS];
+extern const uint16_t ROW_PIN[ROWS];
+extern GPIO_TypeDef* COL_PORT[COLS];
+extern const uint16_t COL_PIN[COLS];
+
+extern char keymap[ROWS][COLS];
+
+void GPIO_Configuration(void);
+void TIM3_Configuration(void);
+void Keyboard_Init(void);
+char Keyboard_GetKey(void);
+
+#endif
