7095ed7d79fd4a5e22f373b373c0adbd023d68ed
linluo-bit <53107393+linluo-bit@users.noreply.github.com>
2023-06-19 08:25:40 +0800

    Create AT24C02.c

---
+++ AT24C02/AT24C02.c
@@ -0,0 +1,186 @@
+#include "AT24c02.h"
+#include "stm32f10x_i2c.h"
+
+ 
+static void delay(unsigned int us) //大概延时 
+{
+    uint8_t i = 10;
+    while(us--)
+    {
+	while(i--);
+    }
+}
+void AT24C02_Init(void)
+{
+   I2C_InitTypeDef  I2C_InitStructure;
+   GPIO_InitTypeDef  GPIO_InitStructure; 
+ 
+   RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1,ENABLE); //使能iic外设时钟
+   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_AFIO , ENABLE); //使能GPIO时钟
+ 
+   /* Configure I2C1 pins: PB6->SCL and PB7->SDA */
+   GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_6 | GPIO_Pin_7;
+   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
+   GPIO_Init(GPIOB, &GPIO_InitStructure);
+		
+   I2C_DeInit(I2C1);
+   I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
+   I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_2;
+   I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;
+   I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+   I2C_InitStructure.I2C_ClockSpeed = 100000;  /* 100K速度 */
+    
+   I2C_Cmd(I2C1, ENABLE);
+   I2C_Init(I2C1, &I2C_InitStructure);
+   /*允许1字节1应答模式*/
+   I2C_AcknowledgeConfig(I2C1, ENABLE);
+}
+ 
+static void I2C_AcknowledgePolling(I2C_TypeDef *I2Cx,uint8_t I2C_Addr)
+{
+  vu16 SR1_Tmp;
+	
+  do
+  {   
+    I2C_GenerateSTART(AT24C02_I2Cx, ENABLE); /*起始位*/
+    /*读SR1*/
+    SR1_Tmp = I2C_ReadRegister(AT24C02_I2Cx, I2C_Register_SR1);
+    /*器件地址(写)*/
+    I2C_Send7bitAddress(AT24C02_I2Cx, I2C_Addr, I2C_Direction_Transmitter);
+ 
+  }while(!(I2C_ReadRegister(AT24C02_I2Cx, I2C_Register_SR1) & 0x0002));
+  
+  I2C_ClearFlag(AT24C02_I2Cx, I2C_FLAG_AF);
+    
+  I2C_GenerateSTOP(AT24C02_I2Cx, ENABLE);  /*停止位*/  
+}
+ 
+ 
+ 
+uint8_t AT24C02_ReadOneByte(uint8_t ReadAddr) //从AT24C02指定的地址读取一个字节
+{
+	uint8_t recvalue;
+	
+	while(I2C_GetFlagStatus(AT24C02_I2Cx, I2C_FLAG_BUSY));
+		
+	/*允许1字节1应答模式*/
+	I2C_AcknowledgeConfig(AT24C02_I2Cx, ENABLE);
+ 
+ 
+	/* 发送起始位 */
+      I2C_GenerateSTART(AT24C02_I2Cx, ENABLE);
+      while(!I2C_CheckEvent(AT24C02_I2Cx, I2C_EVENT_MASTER_MODE_SELECT));/*EV5,主模式*/
+      /*发送器件地址(写)*/
+      I2C_Send7bitAddress(AT24C02_I2Cx,  AT24C02_ADDR, I2C_Direction_Transmitter);
+      while (!I2C_CheckEvent(AT24C02_I2Cx,                    I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+    
+	/*发送地址*/
+	I2C_SendData(AT24C02_I2Cx, ReadAddr);
+    while (!I2C_CheckEvent(AT24C02_I2Cx, I2C_EVENT_MASTER_BYTE_TRANSMITTED));/*数据已发送*/
+		
+	/*起始位*/
+	I2C_GenerateSTART(AT24C02_I2Cx, ENABLE);
+	while(!I2C_CheckEvent(AT24C02_I2Cx, I2C_EVENT_MASTER_MODE_SELECT));
+	
+	/*器件读*/
+	I2C_Send7bitAddress(AT24C02_I2Cx, AT24C02_ADDR, I2C_Direction_Receiver);
+	while(!I2C_CheckEvent(AT24C02_I2Cx, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
+	
+   I2C_AcknowledgeConfig(AT24C02_I2Cx, DISABLE);	/* 最后一位后要关闭应答的 */
+   I2C_GenerateSTOP(AT24C02_I2Cx, ENABLE);			/* 发送停止位 */
+	 while(!I2C_CheckEvent(AT24C02_I2Cx, I2C_EVENT_MASTER_BYTE_RECEIVED));  /* EV7 */
+	    recvalue = I2C_ReceiveData(AT24C02_I2Cx);
+			
+	/* 再次允许应答模式 */
+	I2C_AcknowledgeConfig(AT24C02_I2Cx, ENABLE);
+ 
+	return recvalue;
+}	
+ 
+void AT24C02_WriteOneByte(uint8_t WriteAddr, uint8_t DataToWrite)
+ 
+{
+    /* 起始位 */
+  	I2C_GenerateSTART(AT24C02_I2Cx, ENABLE);
+  	while(!I2C_CheckEvent(AT24C02_I2Cx, I2C_EVENT_MASTER_MODE_SELECT));  
+ 
+  	/* 发送器件地址(写)*/
+  	I2C_Send7bitAddress(AT24C02_I2Cx, AT24C02_ADDR, I2C_Direction_Transmitter);
+  	while(!I2C_CheckEvent(AT24C02_I2Cx,     I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+  
+  	/*发送地址*/
+  	I2C_SendData(AT24C02_I2Cx, WriteAddr);
+	 while(!I2C_CheckEvent(AT24C02_I2Cx, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+ 
+	 /* 写一个字节*/
+  	I2C_SendData(AT24C02_I2Cx, DataToWrite); 
+  	while(!I2C_CheckEvent(AT24C02_I2Cx, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+	
+  	/* 停止位*/
+  	I2C_GenerateSTOP(AT24C02_I2Cx, ENABLE);
+  
+  	I2C_AcknowledgePolling(AT24C02_I2Cx,AT24C02_ADDR);
+ 
+	  delay(100);
+ 
+	return ;
+} 
+ 
+void AT24C02_Write(uint8_t WriteAddr,uint8_t *Buffer,uint16_t Num)//向指定地址连续写入过个字节
+{
+	while(Num--)
+	{
+	    AT24C02_WriteOneByte(WriteAddr,*Buffer);
+	    WriteAddr++;
+	    Buffer++;
+	}
+}
+void AT24C02_Read(uint8_t ReadAddr,uint8_t *Buffer,uint16_t Num)//从指定地址连续读取多个字节
+{
+    if(Num==0)
+	return ;
+	
+	while(I2C_GetFlagStatus(AT24C02_I2Cx, I2C_FLAG_BUSY));
+		
+	/*允许1字节1应答模式*/
+	I2C_AcknowledgeConfig(AT24C02_I2Cx, ENABLE);
+ 
+ 
+	/* 发送起始位 */
+  I2C_GenerateSTART(AT24C02_I2Cx, ENABLE);
+  while(!I2C_CheckEvent(AT24C02_I2Cx, I2C_EVENT_MASTER_MODE_SELECT));/*EV5,主模式*/
+  /*发送器件地址(写)*/
+  I2C_Send7bitAddress(AT24C02_I2Cx,  AT24C02_ADDR, I2C_Direction_Transmitter);
+  while (!I2C_CheckEvent(AT24C02_I2Cx, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+ 
+	/*发送地址*/
+	I2C_SendData(AT24C02_I2Cx, ReadAddr);
+    while (!I2C_CheckEvent(AT24C02_I2Cx, I2C_EVENT_MASTER_BYTE_TRANSMITTED));/*数据已发送*/
+		
+	/*起始位*/
+	I2C_GenerateSTART(AT24C02_I2Cx, ENABLE);
+	while(!I2C_CheckEvent(AT24C02_I2Cx, I2C_EVENT_MASTER_MODE_SELECT));
+	
+	/*器件读*/
+	I2C_Send7bitAddress(AT24C02_I2Cx, AT24C02_ADDR, I2C_Direction_Receiver);
+	while(!I2C_CheckEvent(AT24C02_I2Cx, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
+	
+        while (Num){
+	if(Num==1)
+	{
+     	    I2C_AcknowledgeConfig(AT24C02_I2Cx, DISABLE);	/* 最后一位后要关闭应答的 */
+    	    I2C_GenerateSTOP(AT24C02_I2Cx, ENABLE);			/* 发送停止位 */
+	}
+	    
+	while(!I2C_CheckEvent(AT24C02_I2Cx,I2C_EVENT_MASTER_BYTE_RECEIVED));  /* EV7 */
+	  *Buffer = I2C_ReceiveData(AT24C02_I2Cx);
+	  Buffer++;
+	    /* Decrement the read bytes counter */
+	  Num--;
+        }
+	/* 再次允许应答模式 */
+	I2C_AcknowledgeConfig(AT24C02_I2Cx, ENABLE);
+ 
+	return;
+}	
