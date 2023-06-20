76fe147b6c4349fc9022460ef3f2621c34e1327e
linluo-bit <53107393+linluo-bit@users.noreply.github.com>
2023-06-20 20:44:29 +0800

    Update keyboard.h

--- keyboard/keyboard.h
+++ keyboard/keyboard.h
@@ -3,16 +3,19 @@
 
 #include "stm32f10x.h"
 
+#define ROWS 4
+#define COLS 4
+
+extern GPIO_TypeDef* ROW_PORT[ROWS];
+extern const uint16_t ROW_PIN[ROWS];
+extern GPIO_TypeDef* COL_PORT[COLS];
+extern const uint16_t COL_PIN[COLS];
+
+extern char keymap[ROWS][COLS];
+
 void GPIO_Configuration(void);
-char read_key(void);
+void TIM2_Configuration(void);
+void Keyboard_Init(void);
+char Keyboard_GetKey(void);
 
-
-//		char key = read_key();
-//    if (key != '\0') {
-//			OLED_ShowChar(0,0,key,12);
-//			OLED_Refresh();
-//		}
-            // 处理按键逻辑
-            // ...
-
-#endif /* KEYBOARD_H */
+#endif
